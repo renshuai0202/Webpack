552944133b3e7032d40f68eeae7a27a0fc353496
帅帅 <renshuai0202@gmail.com>
2023-05-15 09:55:41 +0800

    feat: 使用force-strict-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,17 @@ module.exports = {
   devServer: {
     static: './dist',
     port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'force-strict-loader',
+          sourceMap: true
+        }
+
+      }
+    ]
   }
 }
